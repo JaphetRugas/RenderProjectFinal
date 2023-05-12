42b07a2aaff92c6a75877cb533a5594cc915c837
JaphetRugas <rugas.jadejaphet.123@gmail.com>
2023-05-12 18:27:53 +0800

    changes in routes

--- app.js
+++ app.js
@@ -22,8 +22,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', loginRouter); 
+
 app.use('/', registerRouter);  
+app.use('/', loginRouter); 
 app.use('/', adminRouter);  
 app.use('/', managerRouter);  
 app.use('/', userRouter);  
