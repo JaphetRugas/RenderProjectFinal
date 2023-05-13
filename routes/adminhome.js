5c0cb02b0fb161be04c3afd87e44a596a1deefb1
JaphetRugas <rugas.jadejaphet.123@gmail.com>
2023-05-13 22:49:15 +0800

    change the flow

--- routes/adminhome.js
+++ routes/adminhome.js
@@ -6,21 +6,67 @@ var prisma = new PrismaClient
 
 /* GET admin page. */
 router.get('/admin/admindashboard', async function(req, res, next) {
-  var users = await prisma.user.findMany()
+  try {
+    const query = req.query.q // Get the value of the 'q' parameter from the query string
+    const users = await prisma.user.findMany()
+    let filteredUsers = users.filter(user => user.usertype === 'Admin')
 
-  res.render('admin/admindashboard', { title: 'Admin', users: users, isEmpty: users.length === 0 });
+    if (query) { // If a search query is provided, filter the results
+      filteredUsers = filteredUsers.filter(user => {
+        const fullName = `${user.firstname} ${user.middlename ? user.middlename + ' ' : ''}${user.lastname}`
+        return fullName.toLowerCase().includes(query.toLowerCase()) || user.email.toLowerCase().includes(query.toLowerCase())
+      })
+    }
+
+    res.render('admin/admindashboard', { title: 'Admin', users: filteredUsers, isEmpty: filteredUsers.length === 0, query: query });
+  } catch (err) {
+    console.error(err)
+    next(err)
+  }
 });
+
+
+
 
 /* GET manager page. */
 router.get('/admin/managerdashboard', async function(req, res, next) {
-  var users = await prisma.User.findMany()
-  res.render('admin/managerdashboard', { title: 'Manager Dashboard', users: users, isEmpty: users.length === 0 });
+  try {
+    const query = req.query.q // Get the value of the 'q' parameter from the query string
+    const users = await prisma.user.findMany()
+    let filteredUsers = users.filter(user => user.usertype === 'Manager')
+
+    if (query) { // If a search query is provided, filter the results
+      filteredUsers = filteredUsers.filter(user => {
+        const fullName = `${user.firstname} ${user.middlename ? user.middlename + ' ' : ''}${user.lastname}`
+        return fullName.toLowerCase().includes(query.toLowerCase()) || user.email.toLowerCase().includes(query.toLowerCase())
+      })
+    }
+
+    res.render('admin/managerdashboard', { title: 'Manager', users: filteredUsers, isEmpty: filteredUsers.length === 0, query: query });
+  } catch (err) {
+    console.error(err)
+    next(err)
+  }
 });
 
 /* GET user page. */
 router.get('/admin/userdashboard', async function(req, res, next) {
-  var users = await prisma.User.findMany()
-  res.render('admin/userdashboard', { title: 'User Dashboard', users: users, isEmpty: users.length === 0 });
-});
+  try {
+    const query = req.query.q // Get the value of the 'q' parameter from the query string
+    const users = await prisma.user.findMany()
+    let filteredUsers = users.filter(user => user.usertype === 'User')
 
+    if (query) { // If a search query is provided, filter the results
+      filteredUsers = filteredUsers.filter(user => {
+        const fullName = `${user.firstname} ${user.middlename ? user.middlename + ' ' : ''}${user.lastname}`
+        return fullName.toLowerCase().includes(query.toLowerCase()) || user.email.toLowerCase().includes(query.toLowerCase())
+      })
+    }
+
+    res.render('admin/userdashboard', { title: 'User', users: filteredUsers, isEmpty: filteredUsers.length === 0, query: query });
+  } catch (err) {
+    console.error(err)
+    next(err)
+  }
+});
 module.exports = router;
